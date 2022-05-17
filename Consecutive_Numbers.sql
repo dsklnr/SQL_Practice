fec01691e01dca85a804e9119dcea94e76ed5145
BenOgle <benogle1@gmail.com>
2022-05-17 10:06:06 -0600

    Commit

---
+++ Consecutive_Numbers.sql
@@ -0,0 +1,2 @@
+select distinct Num as ConsecutiveNums 
+from Logs,(select Id as id2, Num as num2 from Logs,(select Id as id1, Num as num1 from Logs) as table1 where Id = id1 + 1 and Num = num1) as table2 where Id = id2 + 1 and Num = num2
