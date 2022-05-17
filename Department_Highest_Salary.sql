316ac4f426d51d3a3330b82058629a0f40f0433f
BenOgle <benogle1@gmail.com>
2022-05-17 11:20:30 -0600

    Commit

---
+++ Department_Highest_Salary.sql
@@ -0,0 +1,5 @@
+SELECT Department.name AS Department, Employee.name AS Employee, salary
+FROM Employee JOIN Department ON Employee.departmentId = Department.id
+WHERE (Employee.departmentId, salary) IN (SELECT Employee.departmentId, MAX(salary)
+                                         FROM Employee
+                                         GROUP BY departmentId)
